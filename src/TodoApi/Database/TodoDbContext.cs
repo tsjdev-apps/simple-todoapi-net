eb210d1947bb78562a5ed498f2ddf08a3400cf6b
Sebastian Jensen <apps@tsjdev-apps.de>
2023-08-27 13:23:56 +0200

    Add DbContext for TodoItems.

---
+++ src/TodoApi/Database/TodoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Database;
+
+public class TodoDbContext : DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options)
+        : base(options) { }
+
+    public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+}
