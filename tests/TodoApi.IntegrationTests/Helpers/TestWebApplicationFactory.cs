9a9c11957d3804af98db1e185e8c3cea56cd18fb
Sebastian Jensen <apps@tsjdev-apps.de>
2023-08-27 13:41:17 +0200

    Add Integration Test project and some integration tests.

---
+++ tests/TodoApi.IntegrationTests/Helpers/TestWebApplicationFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TodoApi.Database;
+
+namespace TodoApi.IntegrationTests.Helpers;
+
+public class TestWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TodoDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<TodoDbContext>(options =>
+            {
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                options.UseSqlite($"Data Source={Path.Join(path, "TodoApi-Tests.db")}");
+            });
+        });
+
+        return base.CreateHost(builder);
+    }
+}
